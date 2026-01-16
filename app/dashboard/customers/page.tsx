723e767755512516bece7581132ac42bea509246
txphu2302 <phu.tran232@hcmut.edu.vn>
2026-01-16 12:32:53 +0700

    Feat: Complete course

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,13 @@
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Customers | Acme Dashboard',
+  },
+  description: 'Manage your customers in the Acme Dashboard.',
+  metadataBase: new URL('https://nextjs-dashboard-kappa-coral-54.vercel.app/dashboard/customers'),
+};
 export default function Page(){
     return <p>Customers Page</p>;
 }
