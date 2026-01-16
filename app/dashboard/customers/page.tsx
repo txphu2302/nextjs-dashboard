7e48a05f14a1703f203aad27ca5e4d5cc8bdc5aa
txphu2302 <phu.tran232@hcmut.edu.vn>
2026-01-16 13:19:16 +0700

    feat: customer page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,10 +1,22 @@
 import { Metadata } from 'next';
+import { fetchFilteredCustomers } from '@/app/lib/data'
+import CustomersTable from '@/app/ui/customers/table'
  
 export const metadata: Metadata = {
   title: 'Customers',
   description: 'Manage your customers in the Acme Dashboard.',
   metadataBase: new URL('https://nextjs-dashboard-kappa-coral-54.vercel.app/dashboard/customers'),
 };
-export default function Page(){
-    return <p>Customers Page</p>;
+
+export default async function Page(props: {
+    searchParams?: Promise<{
+        query?: string;
+        page?: string;
+    }>;
+}) {
+  const searchParams = await props.searchParams;
+    const query = searchParams?.query || '';
+    const currentPage = Number(searchParams?.page) || 1;
+    const customers = await fetchFilteredCustomers(query);
+    return <CustomersTable customers={customers} query={query} currentPage={currentPage} />;
 }
